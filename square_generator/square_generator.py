18a34124909638c877afebda88e1a81358dc652f
MyronenkoHeorhii <162124497+MyronenkoHeorhii@users.noreply.github.com>
2024-03-24 22:59:39 +0100

    Delete square_generator directory

--- square_generator/square_generator.py
+++
@@ -1,8 +0,0 @@
-import abc
-
-
-class SquareGenerator(abc.ABC):
-    @abc.abstractmethod
-    def generate_squares(self, start, end):
-        squares = [x * x for x in range(start, end)]
-        return squares
